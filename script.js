bae6e9601e965f21b5b235abe792f6194cea35c8
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-01-07 09:43:05 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/** @format */
+
+let arr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+function PrintDeveloperswithMap() {
+  //Write your code here
+}
+
+function PrintDeveloperbyForEach() {
+  //Write your code here
+}
+
+function addData() {
+  //Write your code here
+}
+
+function removeAdmin() {
+  //Write your code here
+}
+
+function concatenateArray() {
+  //Write your code here
+}
