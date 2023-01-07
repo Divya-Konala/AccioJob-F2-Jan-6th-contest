43ee531f884db88d05370ad2b411874ac182a3ff
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-01-07 11:40:51 +0530

    done

--- script.js
+++ script.js
@@ -8,20 +8,52 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  console.log("Fetching developers using map function");
+  arr.map(function (emp,index){
+    if(emp.profession=="developer"){
+      console.log(emp);
+    }
+  })
+
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  console.log("Fetching developers using forEach function");
+  arr.forEach((emp,index) => {
+    if(emp.profession==="developer"){
+      console.log(emp);
+    }
+  })
 }
 
 function addData() {
   //Write your code here
+  let new_emp={id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(new_emp);
+  console.log("New Employee Added:",arr[arr.length-1]);
+  console.log("Updated Array:",arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  arr=arr.filter(function (emp,index){
+    if(emp.profession!=="admin")
+      return emp;
+    else{
+      console.log("Removed Admin:",emp);
+    }
+  })
+  console.log("Updated Array:",arr);
 }
 
 function concatenateArray() {
   //Write your code here
+  let new_arr = [
+    { id: 5, name: "Adam", age: "25", profession: "UI designer" },
+    { id: 6, name: "Betty", age: "21", profession: "tester" },
+    { id: 7, name: "Charlie", age: "23", profession: "consultant" },
+  ];
+  arr=arr.concat(new_arr);
+  console.log("Concatenated Array", arr);
 }
